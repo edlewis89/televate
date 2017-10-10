afd31a65f6449e280d32bfbdf1c2b2f7624545ba
edlewis89 <edlewis89@msn.com>
2017-10-09 23:29:40 -0400

    attempting to fix render of template for json status

--- app/controllers/api/v1/upload_controller.rb
+++ app/controllers/api/v1/upload_controller.rb
@@ -29,10 +29,11 @@ class API::V1::UploadController < ApplicationController
       dsl = CellInfo::Dsl.new(conditions[:device_id], conditions[:cellinfo], conditions[:location], conditions[:ping], conditions[:timestamp])
       if dsl && dsl.extract       
         #json_response(@cell_info, :created)
-        json_response( {status: "success", code: 200, message: "ceated cell upload for device_id #{conditions[:device_id]}"})
+        format.json{ render :json => {status: "success", code: 200, message: "ceated cell upload for device_id #{conditions[:device_id]}"}}
       end
     else
-      json_response({status: "error", code: 422, message: "device_id, cellinfo, location, ping, timestamp is required."})
+      format.json{ render :json => {status: "error", code: 422, message: "device_id, cellinfo, location, ping, timestamp is required."}}      
+      #json_response({status: "error", code: 422, message: "device_id, cellinfo, location, ping, timestamp is required."})
     end
     
   end
