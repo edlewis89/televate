2f5e6150194af6b37d7d98630483a0e8f9b97ae4
edlewis89 <edlewis89@msn.com>
2017-10-09 20:45:51 -0400

    added error handling

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,25 @@
+class ErrorsController < ApplicationController
+  def not_found
+    render json: {
+      status: 404,
+      error: :not_found,
+      message: 'Where did the 403 errors go'
+    }, status: 404
+  end
+
+  def internal_server_error
+    render json: {
+      status: 500,
+      error: :internal_server_error,
+      message: 'Houston we have a problem'
+    }, status: 500
+  end
+  
+  def unprocessable
+    render json: {
+      status: 422,
+      error: :unprocessable_entity,
+      message: 'Unprocessable entity.  Invalid authenticity token'
+    }, status: 422
+  end
+end
