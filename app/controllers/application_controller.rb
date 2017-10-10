3f648be17f7d8cf85a3ad929b6c718cde0aad7b7
edlewis89 <edlewis89@msn.com>
2017-10-10 00:17:48 -0400

    removing protect_from_forgery for api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::API
 
   include Response
   include ExceptionHandler
-  protect_from_forgery prepend: true
+  #protect_from_forgery prepend: true
 end
