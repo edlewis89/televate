e2f281c36377be4a77f3cf3e349502310f84699f
edlewis89 <edlewis89@msn.com>
2017-10-18 23:08:42 -0400

    removed serialize datum as json

--- app/models/ingested_datum.rb
+++ app/models/ingested_datum.rb
@@ -1,5 +1,5 @@
 class IngestedDatum < ApplicationRecord
-  serialize :data, JSON
+  #serialize :data, JSON
   has_many :ingested_data_metrics, dependent: :destroy, :class_name => 'IngestedDataMetrics'
   has_many :metrics, through: :ingested_data_metrics
 end
