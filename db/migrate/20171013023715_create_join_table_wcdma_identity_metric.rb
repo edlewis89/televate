67e0bd6f4ee3c7b78ff29c2d762c99ac3efed319
edlewis89 <edlewis89@msn.com>
2017-11-28 15:28:13 -0500

    Combined Identity and SS

--- db/migrate/20171013023715_create_join_table_wcdma_identity_metric.rb
+++ db/migrate/20171013023715_create_join_table_wcdma_identity_metric.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableWcdmaIdentityMetric < ActiveRecord::Migration[5.1]
   def change
-    #create_join_table :wcdma_identities, :metrics do |t|
+    create_join_table :wcdma_identities, :metrics do |t|
       # t.index [:wcdma_identity_id, :metric_id]
       # t.index [:metric_id, :wcdma_identity_id]
-    #end
+    end
   end
 end
