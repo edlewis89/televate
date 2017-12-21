936784d15233bf21d65a408364f65210f6682102
edlewis89 <edlewis89@msn.com>
2017-12-21 00:04:30 -0500

    Fix error with escaped quotes

--- db/migrate/20171011041657_create_lte_identity.rb
+++ db/migrate/20171011041657_create_lte_identity.rb
@@ -18,8 +18,6 @@ class CreateLteIdentity < ActiveRecord::Migration[5.1]
       t.integer :mtimingadvance, :limit => 8      
       t.datetime :mtimestamp
      
-     [{"mCellIdentityLte":{"mCi":30025238,"mEarfcn":0,"mMcc":311,"mMnc":480,"mPci":271,"mTac":29952},"mCellSignalStrengthLte":{"mCqi":2147483647,"mRsrp":-90,"mRsrq":-9,"mRssnr":2147483647,"mSignalStrength":26,"mTimingAdvance":4},"mRegistered":true,"mTimeStamp":183467365739448,"mTimeStampType":3},{"mCellIdentityLte":{"mCi":2147483647,"mEarfcn":0,"mMcc":2147483647,"mMnc":2147483647,"mPci":195,"mTac":2147483647},"mCellSignalStrengthLte":{"mCqi":2147483647,"mRsrp":-96,"mRsrq":-13,"mRssnr":2147483647,"mSignalStrength":20,"mTimingAdvance":4},"mRegistered":false,"mTimeStamp":183467365739448,"mTimeStampType":3},{"mCellIdentityLte":{"mCi":2147483647,"mEarfcn":0,"mMcc":2147483647,"mMnc":2147483647,"mPci":107,"mTac":2147483647},"mCellSignalStrengthLte":{"mCqi":2147483647,"mRsrp":-103,"mRsrq":-19,"mRssnr":2147483647,"mSignalStrength":20,"mTimingAdvance":4},"mRegistered":false,"mTimeStamp":183467365739448,"mTimeStampType":3}]
-
      
       t.timestamps
     end
